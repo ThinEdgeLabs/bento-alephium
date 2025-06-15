7934242992c2a2a6028c3ea5d930dc70a8224073
Radu Ciobanu <radu.ciobanu22@gmail.com>
2025-06-15 21:52:12 +0800

    address from contract id util

--- examples/linx-indexer/src/bin/main.rs
+++ examples/linx-indexer/src/bin/main.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 
 use linx_indexer::{
-    processors::contract_call_processor, processors::transfer_processor,
+    processors::contract_call_processor, processors::dex_processor, processors::transfer_processor,
     routers::AccountTransactionApiModule,
 };
 
@@ -13,6 +13,7 @@ async fn main() -> anyhow::Result<()> {
     processor_factories.insert("transfers".to_string(), transfer_processor::processor_factory());
     processor_factories
         .insert("contract_calls".to_string(), contract_call_processor::processor_factory());
+    processor_factories.insert("dex".to_string(), dex_processor::processor_factory());
     let router = Some(AccountTransactionApiModule::register());
     bento_cli::run_command(processor_factories, router, true).await?;
 
