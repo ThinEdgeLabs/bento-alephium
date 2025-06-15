17e1473ad901f6d871c31914c046642f2d08fcaa
Radu Ciobanu <radu.ciobanu22@gmail.com>
2025-06-15 21:54:25 +0800

    Pool repository

--- examples/linx-indexer/src/repository.rs
+++ examples/linx-indexer/src/repository.rs
@@ -1,3 +1,5 @@
 pub mod account_transactions_repository;
+pub mod pool_repository;
 
 pub use account_transactions_repository::AccountTransactionRepository;
+pub use pool_repository::PoolRepository;
